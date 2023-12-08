2a77f86f1543a5a1fee14d55824779cd494e5c17
Dirk Van Haerenborgh <vhdirk@gmail.com>
2023-12-08 14:09:56 +0100

    add datalink frame

---
+++ src/link/frame.rs
@@ -0,0 +1,41 @@
+use deku::prelude::*;
+
+use crate::{network::{AddressType, Address, self}, app::operand::Length};
+
+#[derive(DekuRead, DekuWrite, Default, Debug, Clone, PartialEq)]
+pub struct BackgroundFrameControl {
+    address_type: AddressType,
+
+    #[deku(bits = 6)]
+    tag_id: u8,
+}
+
+#[derive(DekuRead, DekuWrite, Default, Debug, Clone, PartialEq)]
+pub struct BackgroundFrame {
+    subnet: u8,
+    control: BackgroundFrameControl,
+    payload: u16,
+    crc16: u16,
+}
+
+#[derive(DekuRead, DekuWrite, Default, Debug, Clone, PartialEq)]
+pub struct ForegroundFrameControl {
+    address_type: AddressType,
+
+    #[deku(bits = 6)]
+    eirp_index: u8,
+}
+
+#[derive(DekuRead, DekuWrite, Default, Debug, Clone, PartialEq)]
+pub struct ForegroundFrame {
+    length: Length,
+    subnet: u8,
+    control: ForegroundFrameControl,
+
+    #[deku(ctx="control.address_type")]
+    target_address: Address,
+
+    #[deku(ctx="Into::<u32>::into(*length)")]
+    frame: network::Frame,
+    crc16: u16,
+}
