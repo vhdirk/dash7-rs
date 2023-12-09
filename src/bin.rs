0e5c8311f61b2219eb03d43c67301b3764a66156
Dirk Van Haerenborgh <vhdirk@gmail.com>
2023-12-09 08:10:11 +0100

    complete cli

--- src/bin.rs
+++ src/bin.rs
@@ -1,4 +1,5 @@
-use clap::{Args, Parser, Subcommand};
+use clap::{Args, Parser, Subcommand, ValueEnum};
+use deku::DekuError;
 use lib::{
     app::command::Command,
     file::{File, FileId},
@@ -18,29 +19,27 @@ enum Commands {
     Parse(ParseArgs),
 }
 
-#[derive(Debug, Args, Clone)]
-#[group(required = true, multiple = false)]
-struct ParseTypeArgs {
-    #[clap(long = "fg", short = 'f', action)]
-    dll_foreground: bool,
-
-    #[clap(long = "bg", short = 'b', action)]
-    dll_background: bool,
-
-    #[clap(long = "alp", short = 'a', action)]
-    alp: bool,
-
-    #[clap(long = "serial", short = 's', action)]
-    serial: bool,
-
-    #[clap(long = "systemfile", short = 'g', action)]
-    systemfile: Option<u8>,
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum ParseType {
+    #[clap(alias = "fg")]
+    Foreground,
+    #[clap(alias = "bg")]
+    Background,
+    #[clap(alias = "a")]
+    Alp,
+    #[clap(alias = "e")]
+    Serial,
+    #[clap(alias = "s")]
+    Systemfile,
 }
 
 #[derive(Debug, Args)]
 struct ParseArgs {
-    #[command(flatten)]
-    parse_type: ParseTypeArgs,
+    #[arg(value_enum, short='t', long="type")]
+    parse_type: Option<ParseType>,
+
+    #[arg(short='f')]
+    file_id: Option<u8>,
 
     #[arg()]
     hex: String,
@@ -50,31 +49,86 @@ fn remove_whitespace(s: &str) -> String {
     s.chars().filter(|c| !c.is_whitespace()).collect()
 }
 
-fn parse(args: ParseArgs) {
-    let input = hex::decode(remove_whitespace(&args.hex)).expect("Could not parse input jex");
+fn parse_foreground_frame(input: &[u8]) -> Result<(), DekuError> {
+    let frame = ForegroundFrame::try_from(input)?;
+    println!("{:?}", frame);
+    Ok(())
+}
 
-    if args.parse_type.dll_foreground {
-        let frame =
-            ForegroundFrame::try_from(input.as_slice()).expect("Could not foreground frame");
-        println!("{:?}", frame);
-        return;
-    } else if args.parse_type.dll_background {
-        let frame =
-            BackgroundFrame::try_from(input.as_slice()).expect("Could not background frame");
-        println!("{:?}", frame);
-        return;
-    } else if args.parse_type.alp {
-        let command = Command::try_from(input.as_slice()).expect("Could not parse command");
-        println!("{}", command);
-        return;
-    } else if args.parse_type.serial {
-        unimplemented!();
-    } else if let Some(file_id_raw) = args.parse_type.systemfile {
-        let file_id: FileId = file_id_raw.try_into().expect("File id invalid");
-        let file =
-            File::from_bytes((input.as_slice(), 0), file_id, 0).expect("Could not parse file");
-        println!("{:?}", file);
-        return;
+fn parse_background_frame(input: &[u8]) -> Result<(), DekuError> {
+    let frame = BackgroundFrame::try_from(input)?;
+    println!("{:?}", frame);
+    Ok(())
+}
+
+fn parse_alp_command(input: &[u8]) -> Result<(), DekuError> {
+    let command = Command::try_from(input)?;
+    println!("{}", command);
+    Ok(())
+}
+
+fn parse_serial(_input: &[u8]) -> Result<(), DekuError> {
+    Err(DekuError::Assertion("Not implemented".to_string()))
+}
+
+fn parse_file(input: &[u8], file_id: FileId) -> Result<(), DekuError> {
+    let file = File::from_bytes((input, 0), file_id, 0)?;
+    println!("{:?}", file);
+    Ok(())
+}
+
+fn parse_any_file(input: &[u8]) -> Result<(), DekuError> {
+    for file_id_raw in 0..=0x2Eu8 {
+        let file_id: FileId = file_id_raw.try_into()?;
+        if parse_file(input, file_id).is_ok() {
+            return Ok(());
+        }
+    }
+    Ok(())
+}
+
+fn parse_any(input: &[u8]) -> Result<(), DekuError> {
+    if parse_foreground_frame(input).is_ok() {
+        return Ok(())
+    }
+    if parse_background_frame(input).is_ok() {
+        return Ok(())
+    }
+    if parse_alp_command(input).is_ok() {
+        return Ok(())
+    }
+    if parse_serial(input).is_ok() {
+        return Ok(())
+    }
+    if parse_any_file(input).is_ok() {
+        return Ok(())
+    }
+
+    Err(DekuError::Parse("Could not parse input".to_string()))
+}
+
+fn parse(args: ParseArgs) {
+    let input_vec = hex::decode(remove_whitespace(&args.hex)).expect("Could not parse input hex");
+    let input = input_vec.as_slice();
+
+    match args.parse_type {
+        Some(ParseType::Foreground) => {
+            parse_foreground_frame(input).expect("Could not foreground frame")
+        }
+        Some(ParseType::Background) => {
+            parse_background_frame(input).expect("Could not foreground frame")
+        }
+        Some(ParseType::Alp) => parse_alp_command(input).expect("Could not parse command"),
+        Some(ParseType::Serial) => parse_serial(input).expect("Could not parse serial"),
+        Some(ParseType::Systemfile) => {
+            if let Some(file_id_raw) = args.file_id {
+                let file_id: FileId = file_id_raw.try_into().expect("File id invalid");
+                parse_file(input, file_id).expect("Could not parse file")
+            } else {
+                parse_any_file(input).expect("Could not parse file")
+            }
+        },
+        None => parse_any(input).expect("Could not parse input")
     }
 }
 
